17fa7f532d77e04641a2064a994fc525386db4ea
Sowmya Viswanathan <sowmya.viswanathans@gmail.com>
2015-06-06 21:42:43 +0530

    Refactored Code to remove white Spaces

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -3,10 +3,8 @@ package com.twu.biblioteca;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
--- test/com/twu/biblioteca/MessagesTest.java
+++ test/com/twu/biblioteca/MessagesTest.java
@@ -1,7 +1,6 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
