7801786afabde0a929c3cdb3232a0ad565585cd5
Sowmya Viswanathan <sowmya.viswanathans@gmail.com>
2015-06-08 20:12:02 +0530

    Refactored Books Class to  intialize book list

--- src/com/twu/biblioteca/Books.java
+++ src/com/twu/biblioteca/Books.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 public class Books {
     private ArrayList<Book> books = new ArrayList();
 
-    Books(ArrayList<Book> books){
-        this.books = books;
+    Books(){
+        books.add(new Book("The Monk Who Sold His Ferrari","Robin Sharma", "2007"));
     }
 
     @Override
     public String toString() {
         String bookList = new String();
         for (Book book : books)
-            bookList += book + "\n";
+            bookList += book.toString() + "\n";
         return bookList;
     }
 }
--- test/com/twu/biblioteca/BooksTest.java
+++ test/com/twu/biblioteca/BooksTest.java
@@ -1,31 +1,18 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
-import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class BooksTest {
 
     @Test
     public void shouldReturnListOfBookDetails() {
-        ArrayList<Book> booksList = new ArrayList<Book>();
 
-        Book firstBookstub = mock(Book.class);
-        when(firstBookstub.toString())
-                .thenReturn("Books");
-
-        Book secondBookstub = mock(Book.class);
-        when(secondBookstub.toString())
-                .thenReturn("Books");
-
-        booksList.add(firstBookstub);
-        booksList.add(secondBookstub);
-        Books books = new Books(booksList);
+        Books books = new Books();
 
         String actualList = books.toString();
 
-        assertEquals("Books\nBooks\n", actualList);
+        assertEquals("The Monk Who Sold His Ferrari, Robin Sharma, 2007\n", actualList);
     }
 }
