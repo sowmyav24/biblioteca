6afa4f5c8e08df9fa9eafc716f75cfafd4ce9c8f
Sowmya Viswanathan <sowmya.viswanathans@gmail.com>
2015-06-09 06:58:13 +0530

    Added hashMap to map option number and class called in Menu and passed the Spec

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -1,9 +1,21 @@
 package com.twu.biblioteca;
 
+import java.util.HashMap;
+
 public class Menu {
+    HashMap<Integer, MenuActionPerformed> menuItems = new HashMap<Integer, MenuActionPerformed>();
+
+    public Menu() {
+        menuItems.put(1, new ListBooks());
+    }
+
     public String returnMenu() {
         return "Menu\n1.List Books";
     }
 
-
+    public void selectOption(MenuView menuView) {
+        int optionNo = menuView.readMenuOption();
+        menuItems.get(optionNo).compute();
+    }
 }
+
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -1,12 +1,26 @@
 package com.twu.biblioteca;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MenuTest {
+    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
 
     @Test
     public void shouldReturnMenuString(){
@@ -15,5 +29,22 @@ public class MenuTest {
         String actualMessage = menu.returnMenu();
 
         assertThat(actualMessage, is(equalTo("Menu\n" +"1.List Books")));
+    }
+
+    @Test
+    public void shouldDispalyListOfBooksWhenListAllBooksIsSelected() {
+        Menu menu = new Menu();
+        MenuView menuViewStub = mock(MenuView.class);
+        when(menuViewStub.readMenuOption())
+                .thenReturn(1);
+        menu.selectOption(menuViewStub);
+
+        assertEquals("The Monk Who Sold His Ferrari, Robin Sharma, 2007\n", outContent.toString());
+
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
     }
 }
