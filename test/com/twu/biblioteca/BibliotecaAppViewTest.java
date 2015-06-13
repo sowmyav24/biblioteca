9bf2c26753bde54f269e4455298177aad1fd2be6
Sowmya Viswanathan <sowmya.viswanathans@gmail.com>
2015-06-13 22:50:17 +0530

    Modified Spec For BibliotecaAppView test

--- test/com/twu/biblioteca/BibliotecaAppViewTest.java
+++ test/com/twu/biblioteca/BibliotecaAppViewTest.java
@@ -4,14 +4,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class BibliotecaAppViewTest {
     final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private ByteArrayInputStream byteArrayInputStream;
 
     @Before
     public void setUpStreams() {
@@ -25,6 +29,20 @@ public class BibliotecaAppViewTest {
         bibliotecaAppView.displayMessage(Message.WELCOME_MESSAGE);
 
         assertEquals("Welcome to BIBLIOTECA\n", outContent.toString());
+    }
+
+    @Test
+    public void shouldAcceptInputFromUser() {
+        String inputData = "1";
+        BibliotecaAppView bibliotecaAppView = mock(BibliotecaAppView.class);
+        when(bibliotecaAppView.readInput())
+                .thenReturn("1");
+        byteArrayInputStream = new ByteArrayInputStream(inputData.getBytes());
+        System.setIn(byteArrayInputStream);
+
+        String actualOption=bibliotecaAppView.readInput();
+
+        assertEquals(inputData, actualOption);
     }
 
     @After
