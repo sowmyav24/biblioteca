87c80478e0234551001a36eb2381fe4ea6a87b9f
Sowmya Viswanathan <sowmya.viswanathans@gmail.com>
2015-06-15 01:00:19 +0530

    Added Spec for invalid type of book checkout

--- test/com/twu/biblioteca/BooksTest.java
+++ test/com/twu/biblioteca/BooksTest.java
@@ -29,7 +29,18 @@ public class BooksTest {
 
         assertEquals(true, actualList);
     }
-    
+
+    @Test
+    public void shouldReturnFalseIfBookIsNotAvailable() {
+        HashMap<Book, Boolean> bookList = new HashMap<>();
+        bookList.put(new Book("The Monk Who Sold His Ferrari", "Robin Sharma", "2007"), true);
+        Books books = new Books(bookList);
+
+        Boolean actualList = books.checkoutBook(new Book("The  Who Sold His Ferrari", " ", " "));
+
+        assertEquals(false, actualList);
+    }
+
     @Test
     public void shouldReturnIfBookCanBeReturned() {
         HashMap<Book, Boolean> bookList = new HashMap<>();
