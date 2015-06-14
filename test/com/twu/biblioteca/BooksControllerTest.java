7885c9f01bdf665d4d1e5167bcee16b7e60cd699
Sowmya Viswanathan <sowmya.viswanathans@gmail.com>
2015-06-15 01:08:41 +0530

    Added Spec for unsuccessfull Return of the book

--- test/com/twu/biblioteca/BooksControllerTest.java
+++ test/com/twu/biblioteca/BooksControllerTest.java
@@ -41,6 +41,23 @@ public class BooksControllerTest {
     }
 
     @Test
+    public void shouldReturnMessageForUnSuccesfullCheckoutBook() {
+        BookItemTokenizer bookItemTokenizer = new BookItemTokenizer();
+        BibliotecaAppView bibliotecaAppViewStub = mock(BibliotecaAppView.class);
+        when(bibliotecaAppViewStub.readInput())
+                .thenReturn("The Monk Who Sold His Ferrari");
+        HashMap<Book, Boolean> bookList = new HashMap<>();
+        bookList.put(new Book("The Monk Who Sold His Ferrari", "Robin Sharma", "2007"), false);
+        Books books = new Books(bookList);
+        BooksController booksController = new BooksController(bibliotecaAppViewStub, books, bookItemTokenizer);
+
+        booksController.checkout();
+
+        verify(bibliotecaAppViewStub, times(1)).displayMessage(Message.UNSUCCESSFULL_CHECKOUT);
+
+    }
+
+    @Test
     public void shouldReturnBook() {
         BookItemTokenizer bookItemTokenizer = new BookItemTokenizer();
         BibliotecaAppView bibliotecaAppViewStub = mock(BibliotecaAppView.class);
@@ -54,6 +71,23 @@ public class BooksControllerTest {
         booksController.returnBook();
 
         verify(bibliotecaAppViewStub, times(1)).displayMessage(Message.SUCCESSFULL_RETURN);
+
+    }
+
+    @Test
+    public void shouldReturnUnsuccessfullMessageForUnsuccessfullReturnBook() {
+        BookItemTokenizer bookItemTokenizer = new BookItemTokenizer();
+        BibliotecaAppView bibliotecaAppViewStub = mock(BibliotecaAppView.class);
+        when(bibliotecaAppViewStub.readInput())
+                .thenReturn("The Monk Who Sold His Ferrari");
+        HashMap<Book, Boolean> bookList = new HashMap<>();
+        bookList.put(new Book("The Monk Who Sold His Ferrari", "Robin Sharma", "2007"),true);
+        Books books = new Books(bookList);
+        BooksController booksController = new BooksController(bibliotecaAppViewStub, books, bookItemTokenizer);
+
+        booksController.returnBook();
+
+        verify(bibliotecaAppViewStub, times(1)).displayMessage(Message.UNSUCCESSFULL_RETURN);
 
     }
 }
