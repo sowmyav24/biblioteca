c4c44582887fa0598a24aefa05c6085c47cde170
Sowmya Viswanathan <sowmya.viswanathans@gmail.com>
2015-06-16 20:40:29 +0530

    Created MovieController to return list of all movies

--- src/com/twu/biblioteca/MovieController.java
+++ src/com/twu/biblioteca/MovieController.java
@@ -1,4 +1,18 @@
 package com.twu.biblioteca;
 
 public class MovieController {
+
+    private BibliotecaAppView bibliotecaAppView;
+    private LibrarySection librarySection;
+
+    public MovieController(BibliotecaAppView bibliotecaAppView, LibrarySection librarySection) {
+        this.bibliotecaAppView = bibliotecaAppView;
+        this.librarySection = librarySection;
+    }
+
+    public void returnListOfAllMovies() {
+        String listOfMovies = librarySection.toString();
+        bibliotecaAppView.displayMessage(listOfMovies);
+    }
 }
+
--- test/com/twu/biblioteca/MovieControllerTest.java
+++ test/com/twu/biblioteca/MovieControllerTest.java
@@ -1,5 +1,23 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
 public class MovieControllerTest {
+
+    @Test
+    public void shouldReturnListOfMovies() {
+        LibrarySection librarySectionStub = mock(LibrarySection.class);
+        when(librarySectionStub.toString())
+                .thenReturn("Movie List");
+
+        BibliotecaAppView bibliotecaAppViewStub = mock(BibliotecaAppView.class);
+        MovieController movieController = new MovieController(bibliotecaAppViewStub, librarySectionStub);
+
+        movieController.returnListOfAllMovies();
+
+        verify(bibliotecaAppViewStub, times(1)).displayMessage("Movie List");
+    }
 
 }
