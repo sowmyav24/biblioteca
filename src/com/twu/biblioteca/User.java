03df0afa4967af5df753070e5c662a7f0e5b899d
Sowmya Viswanathan <sowmya.viswanathans@gmail.com>
2015-06-17 14:04:51 +0530

    Created user class and its Specs

---
+++ src/com/twu/biblioteca/User.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+public class User {
+    private String name;
+    private String email;
+    private String phone;
+    private String logid;
+    private String pwd;
+
+    public User(String name, String email, String phone, String logid, String pwd) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.logid = logid;
+        this.pwd = pwd;
+    }
+
+    public String returnDetails() {
+        return name + "," + email + "," + phone;
+    }
+}
--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -1,5 +1,16 @@
 package com.twu.biblioteca;
 
-public class UserTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+    @Test
+    public void shouldReturnUserDetails() {
+        User user = new User("Name", "email", "phone", "logid", "pwd");
+
+        String userDetails = user.returnDetails();
+
+        assertEquals("Name,email,phone", userDetails);
+    }
 }
